52341b6db82445bacc68770bfb122ed2d7afc96a
JASTILATHASRI <162718079+JASTILATHASRI@users.noreply.github.com>
2025-03-18 23:42:17 +0530

    Update cronScheduler.js

--- backend/config/cronScheduler.js
+++ backend/config/cronScheduler.js
@@ -1,38 +1,9 @@
 const cron = require('node-cron');
-const User = require('../models/User');
 const { sendMedicineReminders } = require('../cron-jobs/sendReminders');
-const moment = require('moment');
-const scheduleReminderJobs = async () => {
-  try {
-    const users = await User.find({ 'medicines.reminderTime': { $exists: true } });
 
-    users.forEach(user => {
-      user.medicines.forEach(medicine => {
-        if (medicine.reminderTime) {
-          const [hour, minute] = medicine.reminderTime.split(':');
-          const cronExpression = `${minute} ${hour} * * *`; 
-
-          console.log(`Scheduling cron job for ${user.username} to take ${medicine.medicineName} at ${medicine.reminderTime}`);
-          console.log(cronExpression)
-          
-          cron.schedule(cronExpression, async () => {
-            console.log(`Cron job triggered: Sending reminder for ${user.username} for medicine ${medicine.medicineName} at ${medicine.reminderTime}...`);
-            
-            try {
-              await sendMedicineReminders(user, medicine);  
-              console.log(`Reminder successfully sent to ${user.username} for ${medicine.medicineName}`);
-            } catch (err) {
-              console.error('Error while sending reminder:', err);
-            }
-          });
-        }
-      });
-    });
-  } catch (err) {
-    console.error('Error scheduling reminder jobs:', err);
-  }
-};
-
-scheduleReminderJobs();
-
-module.exports = { scheduleReminderJobs };
+// Schedule the cron job to run once a day (e.g., at 8:00 AM)
+console.log("reminder")
+cron.schedule('0 8 * * *', () => {
+  console.log('Running the medicine reminder cron job...');
+  sendMedicineReminders();
+});

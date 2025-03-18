349d28b977e937b44068e9ffb98e34c0da5d164d
JASTILATHASRI <162718079+JASTILATHASRI@users.noreply.github.com>
2025-03-18 23:51:33 +0530

    Update cronScheduler.js

--- backend/config/cronScheduler.js
+++ backend/config/cronScheduler.js
@@ -3,7 +3,7 @@ const { sendMedicineReminders } = require('../cron-jobs/sendReminders');
 
 // Schedule the cron job to run once a day (e.g., at 8:00 AM)
 console.log("reminder")
-cron.schedule('* * * * *', () => {
+cron.schedule('12 12 * * *', () => {
   console.log('Running the medicine reminder cron job...');
   sendMedicineReminders();
 });
